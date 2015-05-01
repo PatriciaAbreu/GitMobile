5123a89bb4845e08092b4078779ebd6e023c265a
PatriciaAbreu <PatriciaAbreu@MacBook-Pro-de-Patricia.local>
2015-05-01 18:55:51 -0300

    Espera quase dando certo

--- GitMobile/GitMobile/ViewController.swift
+++ GitMobile/GitMobile/ViewController.swift
@@ -8,23 +8,42 @@
 
 import UIKit
 
+
+
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
+    @IBOutlet weak var labelMessage: UILabel!
     
     var git: GitManager = GitManager()
     let animation = CABasicAnimation(keyPath: "position")
     var nextView:UIViewController!
     
     @IBOutlet weak var userTextField: UITextField!
+    
+    @IBOutlet weak var btnLogin: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-            userTextField.placeholder = "Digite o nome de Usuário do Git..."
+        userTextField.placeholder = "Digite o nome de Usuário do Git..."
+        activityIndicator.hidden = true
+        labelMessage.hidden = true
     }
     
     override func viewWillAppear(animated: Bool) {
-            }
+    }
     
     @IBAction func entradaUsuario(sender: AnyObject) {
+        
+        activityIndicator.hidden = false
+        labelMessage.hidden = false
+        userTextField.hidden = true
+        btnLogin.hidden = true
+        activityIndicator.startAnimating()
+        activityIndicator.hidesWhenStopped = true
+        labelMessage.text = "Fazendo login..."
         
         if userTextField.text == "" {
             animation.duration = 0.07
@@ -36,14 +55,17 @@ class ViewController: UIViewController {
         }
         else{
             
+            
+            
             var valida = git.buscarRepositorio(userTextField.text)
             
             if valida == -1{
-            
+                
                 let alerta: UIAlertController = UIAlertController(title: "Usuário não encontrado", message: "Digite seu usuário novamente", preferredStyle: .ActionSheet)
                 
                 let acao: UIAlertAction = UIAlertAction(title: "OK", style: .Default, handler: { action -> Void in
-                    
+                    self.userTextField.hidden = false
+                    self.btnLogin.hidden = false
                 })
                 
                 alerta.addAction(acao)
@@ -52,43 +74,91 @@ class ViewController: UIViewController {
             }
             else{
                 nextView = self.storyboard!.instantiateViewControllerWithIdentifier("MainNavigationController") as! UIViewController
-//------------------ INICIO DA VIEW DE ESPERA
-//                var alertView: UIAlertView = UIAlertView(title: "Carregando...", message: nil, delegate: nil, cancelButtonTitle: nil)
-//                var activityIndicator: UIActivityIndicatorView! = UIActivityIndicatorView(frame: CGRectMake(50, 0, 35, 35)) as UIActivityIndicatorView
-//                activityIndicator.center = self.view.center
-//                activityIndicator.hidesWhenStopped = true
-//                activityIndicator.activityIndicatorViewStyle = UIActivityIndicatorViewStyle.Gray
-//                activityIndicator.startAnimating()
-//                UIApplication.sharedApplication().networkActivityIndicatorVisible = true
-//                
-//                alertView.setValue(activityIndicator, forKey: "accessoryView")
-//                activityIndicator.startAnimating()
-//                
-//                alertView.show()
-//                
-//                let priority = DISPATCH_QUEUE_PRIORITY_DEFAULT
-//                dispatch_async(dispatch_get_global_queue(priority, 0), { ()->() in
-//                    dispatch_async(dispatch_get_main_queue(), {
-//                        RepositorioManager.sharedInstance.removerTodos()
-//                        RepositorioManager.sharedInstance.buscarRepositorio()
-//                        dispatch_async(dispatch_get_main_queue(), {
-//             //               self.nextView.tableView.reloadData() ---- dar uma jeito para pegar a tableView
-//                            alertView.title = "Finalizado"
-//                            activityIndicator.stopAnimating()
-//                            UIApplication.sharedApplication().networkActivityIndicatorVisible = false
-//                        })
-//                        sleep(1)
-//                        dispatch_async(dispatch_get_main_queue(), {
-//                            alertView.dismissWithClickedButtonIndex(-1, animated: true)
-//                        })
-//
-//                    })
-//                    
-//                })
-//------------------- FIM CODIGO DA VIEW DE ESPERA
                 
-                NSUserDefaults().setObject(userTextField.text, forKey: "usuario")
-                self.presentViewController(nextView, animated: true, completion: { () -> Void in})
+                //------------------ INICIO DA VIEW DE ESPERA
+                
+                
+                let priority = DISPATCH_QUEUE_PRIORITY_DEFAULT
+                dispatch_async(dispatch_get_global_queue(priority, 0), { ()->() in
+                    dispatch_async(dispatch_get_main_queue(), {
+                        //                        self.activityIndicator.hidden = false
+                        //                        self.labelMessage.hidden = false
+                        //                        self.userTextField.hidden = true
+                        //                        self.btnLogin.hidden = true
+                        //                        self.activityIndicator.startAnimating()
+                        //                        self.activityIndicator.hidesWhenStopped = true
+                        //                        self.labelMessage.text = "Fazendo login..."
+                    })
+                    //Acao de login
+                    sleep(1)
+                    
+                    dispatch_async(dispatch_get_main_queue(), {
+                        NSUserDefaults().setObject(self.userTextField.text, forKey: "usuario")
+                        
+                        self.labelMessage.text = "Usuario logado\nCarregando dados..."
+                    })
+                    
+                    //Acao de carregar os dados
+                    sleep(2)
+                    
+                    //                    dispatch_async(dispatch_get_main_queue(), {
+                    //                        self.labelMessage.text = "Dados carregandos... preparando aplicação"
+                    //                    })
+                    
+                    //prepara os dados para a proxima tela
+                    // chama a proxima tela
+                    println("FIM da Thread")
+                    
+                    
+                    dispatch_async(dispatch_get_main_queue(), {
+                        self.presentViewController(self.nextView, animated: true, completion: { () -> Void in})
+                        self.activityIndicator.stopAnimating()
+                    })
+                    
+                })
+                println("Fim do ViewDidLoad")
+                
+                
+                
+                
+                
+                
+                //                var alertView: UIAlertView = UIAlertView(title: "Carregando...", message: nil, delegate: nil, cancelButtonTitle: nil)
+                //                var activityIndicator: UIActivityIndicatorView! = UIActivityIndicatorView(frame: CGRectMake(50, 0, 35, 35)) as UIActivityIndicatorView
+                //                activityIndicator.center = self.view.center
+                //                activityIndicator.hidesWhenStopped = true
+                //                activityIndicator.activityIndicatorViewStyle = UIActivityIndicatorViewStyle.Gray
+                //                activityIndicator.startAnimating()
+                //                UIApplication.sharedApplication().networkActivityIndicatorVisible = true
+                //
+                //                alertView.setValue(activityIndicator, forKey: "accessoryView")
+                //                activityIndicator.startAnimating()
+                //
+                //                alertView.show()
+                //
+                //                let priority = DISPATCH_QUEUE_PRIORITY_DEFAULT
+                //                dispatch_async(dispatch_get_global_queue(priority, 0), { ()->() in
+                //                    dispatch_async(dispatch_get_main_queue(), {
+                //                        RepositorioManager.sharedInstance.removerTodos()
+                //                        RepositorioManager.sharedInstance.buscarRepositorio()
+                //                        dispatch_async(dispatch_get_main_queue(), {
+                //             //               self.nextView.tableView.reloadData() ---- dar uma jeito para pegar a tableView
+                //                            alertView.title = "Finalizado"
+                //                            activityIndicator.stopAnimating()
+                //                            UIApplication.sharedApplication().networkActivityIndicatorVisible = false
+                //                        })
+                //                        sleep(1)
+                //                        dispatch_async(dispatch_get_main_queue(), {
+                //                            alertView.dismissWithClickedButtonIndex(-1, animated: true)
+                //                        })
+                //
+                //                    })
+                //
+                //                })
+                //------------------- FIM CODIGO DA VIEW DE ESPERA
+                
+                //                NSUserDefaults().setObject(userTextField.text, forKey: "usuario")
+                //                self.presentViewController(nextView, animated: true, completion: { () -> Void in})
                 
             }
         }
